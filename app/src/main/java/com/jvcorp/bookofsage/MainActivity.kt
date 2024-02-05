3a7f8bf7181c3378220859c4350b663df76f7948
jhvillafuerte <jhvillafuerte@gmail.com>
2024-02-05 12:46:59 +0800

    update main screen

--- app/src/main/java/com/jvcorp/bookofsage/MainActivity.kt
+++ app/src/main/java/com/jvcorp/bookofsage/MainActivity.kt
@@ -1,9 +1,19 @@
 package com.jvcorp.bookofsage
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -11,6 +21,14 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.jvcorp.bookofsage.ui.theme.BookOfSageTheme
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Button
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.draw.paint
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,25 +40,29 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Box(
+                        modifier = with (Modifier){
+                            fillMaxSize()
+                                .paint(
+                                    // Replace with your image id
+                                    painterResource(id = R.drawable.bookofsage3),
+                                    contentScale = ContentScale.FillHeight)
+
+                        })
+                    {
+                        Spacer(modifier = Modifier.width(8.dp))
+                        Column (modifier = Modifier.align(Alignment.Center)) {
+                            Image(painter = painterResource(id = R.drawable.bos_main_title),
+                                contentDescription = "title")
+                            
+                            Spacer(modifier = Modifier.height(4.dp))
+                            Button(onClick = { /*TODO*/ }) {
+                                Text(text = "Enter")
+                            }
+                        }
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    BookOfSageTheme {
-        Greeting("Android")
     }
 }
